a2e0e145cfa4feb31dcd6584fd001b4ae4504f88
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-05-28 11:17:02 -0700

    Implement TreeNode, add tree creation

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,22 @@
 public class TreeNode {
+    public int bleepbloop;
+    public TreeNode left;
+    public TreeNode right;
 
-  
+    public TreeNode(int bleepbloop) {
+        this.bleepbloop = bleepbloop;
+    }
+
+    public TreeNode(int bleepbloop, TreeNode left) {
+        this.bleepbloop = bleepbloop;
+        this.left = left;
+    }
+
+    public TreeNode(int bleepbloop, TreeNode left, TreeNode right) {
+        this.bleepbloop = bleepbloop;
+        this.left = left;
+        this.right = right;
+    }
+
+    //TreeNode(8, null, TreeNode(5))
 }
